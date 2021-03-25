74022c370a27b1eb7acf8a2989d9a45852824cb8
danish.amjad <danish.amjad@gaditek.com>
2021-03-25 14:13:08 +0500

    - added intent function for data passing.

--- savyourlibrary/src/main/java/com/android/savyourlibrary/SavyourTracker.java
+++ savyourlibrary/src/main/java/com/android/savyourlibrary/SavyourTracker.java
@@ -1,7 +1,9 @@
 package com.android.savyourlibrary;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
+import android.util.Log;
 
 public class SavyourTracker
 {
@@ -10,5 +12,33 @@ public class SavyourTracker
     public  void showConnectedMessage(Context c, String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+
+    public void initData(Intent intent)
+    {
+        if(intent!=null)
+        {
+            Log.e(TAG,"Intent is not null");
+
+            if(intent.getAction().equalsIgnoreCase("savyour"))
+            {
+                Log.e(TAG,"Is Coming from savyour application");
+                Log.e(TAG,"user Id is: " + intent.getStringExtra("userid"));
+            }
+            else
+            {
+                Log.e(TAG,"Is not Coming from savyour application");
+            }
+        }
+        else
+        {
+            Log.e(TAG,"Intent is null");
+        }
+    }
+
+
+    public  void orderPlaced()
+    {
+            Log.e(TAG,"order Placed");
     }
 }
